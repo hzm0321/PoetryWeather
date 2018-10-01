6add6f63b07308cda895cf5934eb3cca51eee4d6
HZM <934585316@qq.com>
2018-10-01 13:33:16 +0800

    完成了对未来7天预报的数据接入

--- app/src/main/java/cn/hzmeurasia/poetryweather/activity/WeatherActivity.java
+++ app/src/main/java/cn/hzmeurasia/poetryweather/activity/WeatherActivity.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,8 @@ import java.util.List;
 
 import cn.hzmeurasia.poetryweather.MyApplication;
 import cn.hzmeurasia.poetryweather.R;
+import interfaces.heweather.com.interfacesmodule.bean.weather.forecast.Forecast;
+import interfaces.heweather.com.interfacesmodule.bean.weather.forecast.ForecastBase;
 import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
 import interfaces.heweather.com.interfacesmodule.view.HeConfig;
 import interfaces.heweather.com.interfacesmodule.view.HeWeather;
@@ -39,8 +44,9 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView tvTemperature;
     private TextView tvWeather;
     private ImageView imgWeatherIcon;
-
-    private RelativeLayout relativeLayout;
+    private LinearLayout forecastLayout;
+//
+//    private RelativeLayout relativeLayout;
     private Intent intent = null;
     private String cityCode = null;
 
@@ -50,22 +56,22 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.weather_activity);
         //控件初始化
         init();
-        //获取手机屏幕高度
-        DisplayMetrics dm = getResources().getDisplayMetrics();
-        int height = dm.heightPixels;
-        //获取手机状态栏高度
-        int statusBarHeight1 = -1;
-        //获取status_bar_height资源的ID
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            //根据资源ID获取响应的尺寸值
-            statusBarHeight1 = getResources().getDimensionPixelSize(resourceId);
-        }
-        //设置RelativeLayout高度为屏幕高度-状态栏高度
-        relativeLayout = findViewById(R.id.rl);
-        ViewGroup.LayoutParams params = relativeLayout.getLayoutParams();
-        params.height = height-statusBarHeight1;
-        relativeLayout.setLayoutParams(params);
+//        //获取手机屏幕高度
+//        DisplayMetrics dm = getResources().getDisplayMetrics();
+//        int height = dm.heightPixels;
+//        //获取手机状态栏高度
+//        int statusBarHeight1 = -1;
+//        //获取status_bar_height资源的ID
+//        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+//        if (resourceId > 0) {
+//            //根据资源ID获取响应的尺寸值
+//            statusBarHeight1 = getResources().getDimensionPixelSize(resourceId);
+//        }
+//        //设置RelativeLayout高度为屏幕高度-状态栏高度
+//        relativeLayout = findViewById(R.id.rl);
+//        ViewGroup.LayoutParams params = relativeLayout.getLayoutParams();
+//        params.height = height-statusBarHeight1;
+//        relativeLayout.setLayoutParams(params);
 
         //注册和风天气
         HeConfig.init("HE1808181021011344","c6a58c3230694b64b78facdebd7720fb");
@@ -75,6 +81,7 @@ public class WeatherActivity extends AppCompatActivity {
         cityCode = intent.getStringExtra("cityCode");
         heWeather();
 
+
     }
 
     private void init() {
@@ -83,46 +90,92 @@ public class WeatherActivity extends AppCompatActivity {
         tvTemperature = findViewById(R.id.tv_weather_temperature);
         tvWeather = findViewById(R.id.tv_weather);
         imgWeatherIcon = findViewById(R.id.iv_weather_icon);
+        forecastLayout = findViewById(R.id.ll_forecast);
     }
 
     /**
      * 载入和风天气数据
      */
     private void heWeather() {
+        //获取实时天气
         HeWeather.getWeatherNow(this, cityCode, new HeWeather.OnResultWeatherNowBeanListener() {
             @Override
             public void onError(Throwable throwable) {
-                Log.i(TAG, "onError: ",throwable);
-                Toast.makeText(WeatherActivity.this,"天气获取失败",Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "onError: ", throwable);
+                Toast.makeText(WeatherActivity.this, "天气获取失败", Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onSuccess(List<Now> list) {
-                StringBuilder uri = new StringBuilder();
                 StringBuilder temperature = new StringBuilder();
 
                 for (Now now : list) {
                     tvCityName.setText(now.getBasic().getLocation());
                     tvUpdateTime.setText(now.getUpdate().getLoc());
                     temperature.append(now.getNow().getFl())
-                            .append("°");
+                            .append("℃");
                     tvTemperature.setText(temperature.toString());
                     tvWeather.setText(now.getNow().getCond_txt());
-                    uri.append("http://www.hzmeurasia.cn/weather_icon/")
-                            .append(now.getNow().getCond_code())
-                            .append(".png");
-                    Glide.with(MyApplication.getContext())
-                            .load(uri.toString())
-                            .into(imgWeatherIcon);
-
+                    //载入天气图标
+                    loadWeatherIcon(now.getNow().getCond_code(),imgWeatherIcon);
                 }
 
             }
         });
+
+        //获取未来7天天气
+        HeWeather.getWeatherForecast(this, cityCode, new HeWeather.OnResultWeatherForecastBeanListener() {
+            @Override
+            public void onError(Throwable throwable) {
+                Log.i(TAG, "onError: ", throwable);
+                Toast.makeText(WeatherActivity.this, "未来7天天气获取失败", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(List<Forecast> list) {
+                forecastLayout.removeAllViews();
+                Log.d(TAG, "未来7天天气预报 "+list.size());
+                for (Forecast forecast : list) {
+                    for (ForecastBase forecastBase : forecast.getDaily_forecast()) {
+                        View view = LayoutInflater.from(WeatherActivity.this).inflate(R.layout.weather_forecast_item, forecastLayout, false);
+                        TextView dateText = view.findViewById(R.id.tv_date);
+                        TextView infoText = view.findViewById(R.id.tv_info);
+                        TextView maxAndMin = view.findViewById(R.id.tv_max_min_temperature);
+                        ImageView imageView = view.findViewById(R.id.iv_icon);
+                        Log.d(TAG, "7天预报数据: "+forecast.getDaily_forecast().size());
+                        dateText.setText(forecastBase.getDate());
+                        infoText.setText(forecastBase.getCond_txt_d());
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append(forecastBase.getTmp_min())
+                                .append("℃")
+                                .append("-")
+                                .append(forecastBase.getTmp_max())
+                                .append("℃");
+                        maxAndMin.setText(stringBuilder.toString());
+                        loadWeatherIcon(forecastBase.getCond_code_d(),imageView
+                        );
+                        forecastLayout.addView(view);
+                    }
+
+                }
+            }
+        });
+
     }
 
-
+    /**
+     * 载入天气图标
+     * @param code
+     * @param imageView
+     */
+    private void loadWeatherIcon(String code,ImageView imageView) {
+        StringBuilder uri = new StringBuilder();
+        uri.append("http://www.hzmeurasia.cn/weather_icon/")
+                .append(code)
+                .append(".png");
+        Glide.with(MyApplication.getContext()).load(uri.toString()).into(imageView);
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
